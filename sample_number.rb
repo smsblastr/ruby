ecd70da1f4f9f90f1c8963dd24b6e272d245ad69
Chris Ian Fiel <ccfiel@gmail.com>
2014-10-27 09:16:35 +0800

    change to production

--- sample_number.rb
+++ sample_number.rb
@@ -3,11 +3,12 @@ require 'curb'
 
 data = {"user_email" => "ccfiel@surebooked.com", "user_token" => "BUuqDCDPyUoGSvtCoFzX", "mobile_number" => "+639177048787", "message" => "test", "pay" => true};                                                                    
 
-http = Curl::Easy.http_post("http://staging.smsblastr.com/api/v1/send_number", data.to_json ) do |curl|
+http = Curl::Easy.http_post("http://smsblastr.com/api/v1/send_number", data.to_json ) do |curl|
   curl.headers['Accept'] = 'application/json'
   curl.headers['Content-Type'] = 'application/json'
   curl.headers['Api-Version'] = '2.2'
 end
 result =  JSON.parse(http.body_str)
+puts result['success']
 puts result;
 
